6436e6c94f17bdee268b4ea5c3a70c615c9c85fb
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2024-02-02 11:31:15 -0300

    Code Cleaning

--- FileImport.py
+++ FileImport.py
@@ -2,40 +2,36 @@ import pandas as pd
 import math
 from selenium import webdriver
 from time import sleep
-import pywhatkit
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-def send_message(telephone, webdriver):
-    chat_finder = None
-
-    while chat_finder is None:
+def search_text_box_and_send(webdriver ,xpath, text):
+    text_box = None
+    while text_box is None:
         try:
-            #chat_finder = webdriver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[3]/div/div[1]/div/div[2]/div[2]/div/div[1]")
-            chat_finder = webdriver.find_element(By.XPATH, '//div[@contenteditable="true"][@data-tab="3"]')
+            text_box = webdriver.find_element(By.XPATH, xpath)
         except NoSuchElementException:
             sleep(5)
 
-    sleep(10) #WebDriverWait(webdriver, 10).until(
-    # EC.prescence_of_element_located((By.ID, search_xpath))
-    chat_finder.send_keys(tel_value)
+    text_box.clear()
+    text_box.send_keys(text)
     sleep(1)
+    text_box.send_keys(Keys.ENTER)
+    sleep(5)
 
-    chat = webdriver.find_element(By.CLASS_NAME,"_3m_Xw").click()
-    #chat = webdriver.find_element(By.XPATH,"/html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[5]/div/div/div")
-    sleep(1)
-    b = input("Continue")
-    message_box = webdriver.find_element(By.XPATH,"/html/body/div[1]/div/div[2]/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]")
-    message_box.send_keys(message)
-    send_button = webdriver.find_element(By.XPATH, "/html/body/div[1]/div/div[2]/div[4]/div/footer/div[1]/div/span[2]/div/div[2]/div[2]/button")
-    b = input("Continue")
-    send_button.click()
-    sleep(2)
-#/html/body/div[1]/div/div[2]/div[3]/div/div[2]/div[1]/div/div/div[2]/div/div
+def send_message(telephone, webdriver, message):
+    try:
+        search_text_box_and_send(webdriver, '//div[@contenteditable="true"][@data-tab="3"]', telephone)
+        search_text_box_and_send(webdriver, '//div[@contenteditable="true"][@title="Type a message"]', message)
 
-name=""
-message = "test. "
+        print(f"Message sent to {telephone}")
+    except Exception as e:
+        print(f"Error sending message to {telephone}: {str(e)}")
+
+
+name = ""
+base_message = "TEST MSG: Hola @v1"
 filepath = "cn.xls"
 df = pd.read_excel(filepath)
 
@@ -47,7 +43,7 @@ webdriver.maximize_window()
 for index, row in df.iterrows():
     tel_value = row['TEL']
     name_value = row['NAME']
-    message = message + name_value
+    message = base_message.replace(base_message, tel_value, name_value)
     if not math.isnan(tel_value):
         tel_value = int(tel_value)
-        send_message(tel_value, webdriver)
+        send_message(tel_value, webdriver, message)
